e7abd6da2dccc9f529410e9b642da87486d3284d
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-09-10 19:37:30 -0300

    1st commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Separator } from '@/components/ui/separator'
+import Navbar from '@/components/Navbar'
+import { ThemeProvider } from '@/providers/ThemeProvider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +20,33 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html 
+        lang="en" 
+        className={cn(inter.className, "dark")}
+        style={{
+          colorScheme: "dark",
+        }}>
+        <body>
+          <ThemeProvider>
+            <div
+              className='
+              flex
+              min-h-screen
+              w-full
+              flex-col
+              items-center
+              dark:bg-black'
+            >
+              <Navbar />
+              <Separator />
+              <main className='flex flex-grow w-full justify-center items-center dark:bg-neutral-950'>
+                {children}
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { UserButton } from '@clerk/nextjs'
+import Logo from './Logo'
+import ThemeSwitcher from './ThemeSwitcher'
+
+
+export default function Navbar() {
+  return (
+    <nav className='flex w-full items-center justify-between p-4 px-8 h-[60px]'>
+        <Logo />
+        <div className='flex gap-4 items-center'>
+            <UserButton afterSignOutUrl='/' />
+            <ThemeSwitcher />
+        </div>
+    </nav>
+  )
+}
